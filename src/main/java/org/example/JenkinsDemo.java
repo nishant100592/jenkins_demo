95cebe2af2065a6a389b3b74e9cd8b5c25747332
Nishant Dwivedi <nishant100592@gmail.com>
2023-12-23 01:23:47 +0530

    difference function added

--- src/main/java/org/example/JenkinsDemo.java
+++ src/main/java/org/example/JenkinsDemo.java
@@ -9,10 +9,15 @@ public class JenkinsDemo {
         return a*b;
     }
 
+    public int difference(int a, int b) {
+        return b-a;
+    }
+
     public static void main(String[] args) {
         System.out.println("Simple Java code");
         JenkinsDemo obj = new JenkinsDemo();
         System.out.println("Sum is "+obj.sum(10, 20));
+        System.out.println("Difference is "+obj.difference(20, 10));
         System.out.println("Product is "+obj.multiplication(10, 20));
     }
 }

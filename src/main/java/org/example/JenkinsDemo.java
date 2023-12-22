537a983a04350946cf2d0ecb7d211bca84fa7ad2
Nishant Dwivedi <nishant100592@gmail.com>
2023-12-23 01:14:53 +0530

    multiplication function added

--- src/main/java/org/example/JenkinsDemo.java
+++ src/main/java/org/example/JenkinsDemo.java
@@ -5,9 +5,14 @@ public class JenkinsDemo {
         return a+b;
     }
 
+    public int multiplication(int a, int b) {
+        return a*b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Simple Java code");
         JenkinsDemo obj = new JenkinsDemo();
         System.out.println("Sum is "+obj.sum(10, 20));
+        System.out.println("Product is "+obj.multiplication(10, 20));
     }
 }
